b0f9b4ec43e25538d2157613ac0048478e3e4491
Wassim-RF <wassimrifi0313@gmail.com>
2025-11-21 16:10:07 +0100

    J'ai fixe une errore d'ouvrire ajoute d'un emploiyee

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,5 @@
 import {setupEvent} from './event.js';
 import {setupETOR} from './logic/manageRolesRoom.js';
-import {setupDragAndDrop} from './logic/dragAndDrop.js';
 
 setupEvent();
 setupETOR();
